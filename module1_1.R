6ac5702c13ea46327d66b50d998b65f8ece2ee58
Shoemaker <kshoemaker@agnt.unr.edu>
2021-09-10 09:16:27 -0700

    mod1.1

--- module1_1.R
+++ module1_1.R
@@ -33,8 +33,8 @@ data(trees)
 #########
 # explore the data object
 
-summary(trees)
-str(trees)
+summary(trees)    # summary statistics for all variables in data frame
+str(trees)        # summary of the data structure
 
 
 #########
@@ -46,7 +46,7 @@ hist(trees$Height)                       # visualize the distribution of height 
 hist(trees$Girth)                        # visualize the distribution of girth data
 hist(trees$Volume)                       # visualize the distribution of volume data
 
-   # scatterplots:
+   # bivariate scatterplots:
 
 layout(matrix(1:2,nrow=1,byrow = T))      # set graphics device with 2 plots side by side
 plot(trees$Volume~trees$Girth)            # scatterplot of volume against girth
@@ -64,6 +64,13 @@ model1 <- lm(Volume~Girth,data=trees)        # regress Volume on Girth
 summary(model1)    # examine the results
 
 
+######
+# test key assumptions visually
+
+layout(matrix(1:4,nrow=2,byrow=T))  # set up graphics window
+plot(model1)  # run diagnostic plots for our regression
+
+
 #########
 # visualize the results!
 
@@ -73,9 +80,9 @@ pred <- predict(model1,newdata=data.frame(Girth=xvals),interval = "confidence",l
 plot(trees$Volume~trees$Girth,xlab="Girth (inches)",ylab="Volume (cubic feet)",main="Black Cherry",
      xlim=range(xvals),ylim=c(0,100))         # Make a pretty scatterplot
 
-abline(model1,lwd=2,col="green")                # Add the regression line
-lines(xvals,pred[,"upr"],col="green",lty=2)       # Add the upper bound of the confidence interval
-lines(xvals,pred[,"lwr"],col="green",lty=2)       #      ... and the lower bound
+abline(model1,lwd=2,col="brown")              # Add the regression line
+lines(xvals,pred[,"upr"],col="brown",lty=2)       # Add the upper bound of the confidence interval
+lines(xvals,pred[,"lwr"],col="brown",lty=2)       #      ... and the lower bound
 text(10,80,sprintf("Volume = %s + %s*Girth",round(coefficients(model1)[1],1),round(coefficients(model1)[2],1)))       # Add the regression coefficients
 text(10,65,sprintf("p = %s",round(summary(model1)$coefficients[,"Pr(>|t|)"][2],3)))     # Add the p-value
 
@@ -159,9 +166,11 @@ d.mat
 d.mat <- matrix(c(1,2,3,4,5,6),nrow=3,ncol=2,byrow=T)        # create matrix another way (stack rows together)
 d.mat
 
-d.mat <- rbind(c(1,4),c(2,5),c(3,6))        # create matrix another way
+d.mat <- rbind(c(1,4),c(2,5),c(3,6))        # create matrix another way (stacking three vectors)
 d.mat
 
+
+# math with matrices
 
 d.mat + 2
 d.mat/sum(d.mat)
@@ -189,7 +198,7 @@ d.array[1,,]    # view a slice of the array
 #############
 
 d.list <- list()        # create empty list
-d.list[[1]] <- c(1,2,3)     # note the double brackets- this is one way to reference a specified list element. 
+d.list[[1]] <- c(1,2,3)     # note the double brackets- this is one way to reference and define a specified list element. 
 d.list[[2]] <- c(4,5)
 d.list[[3]] <- "Alfred Pennyworth"
 d.list
@@ -234,9 +243,9 @@ help(sum)
 #############
 
 #######
-# Sequences
+# Generating vector sequences
 
-1:10                        # sequence from 1 to 10
+1:10                        # sequential vector from 1 to 10
 10:1                        # reverse the order
 rev(1:10)                   # a different way to reverse the order
 
@@ -248,7 +257,7 @@ seq(0,1,length=10)          # sequence of length 10 between 0 and 1
 
 
 ##############
-# Repeating sequences
+# Repeating vector sequences
 
 rep(0,times=3)                # repeat 0 three times
 rep(1:3,times=2)              # repeat 1:3 two times
@@ -279,7 +288,7 @@ sample(1:10,size=5,replace=TRUE,prob=(1:10)/sum(1:10)) # 5 rvs from discrete pmf
 
 
 ############
-# Make up an entire data frame!
+# Make up an entire fake data frame!
 
 
 my.data <- data.frame(
@@ -298,7 +307,7 @@ summary(my.data)    # Use the "summary()" function to summarize each column in t
 ### Accessing, indexing and subsetting data
 ############
 
-# X[i]         access the ith element of X
+# X[i]         access the ith element of vector X
 
 d.vec <- 2:10
 d.vec
@@ -337,14 +346,14 @@ d.df[["Germination"]]      # same thing!
 d.df[,4]                   # same thing!
 d.df[[4]]                  # same thing!
 d.df[,"Germination"]       # same thing!
-d.df["Germination"]        # same thing!  (or is it...)
+d.df["Germination"]        # same thing!  (or is it...??)
 
 
 d.df$AvgHeight
 d.df$AvgHeight[3]  # subset an element of a data frame
 
 
-# Data frame can be indexed just like a matrix
+# Data frame can also be indexed just like a matrix
 d.df[,2]
 d.df[2,]
 d.df[,2:3]
@@ -362,6 +371,10 @@ names(my.data)    # get names of variables in a data frame (or names of elements
 nrow(my.data)     # get number of rows/observations in a data frame
 ncol(my.data)     # get number of columns/variables in a data frame
 str(my.data)      # look at the "internals" of an object (useful for making sense of complex objects!)
+
+
+########
+# basic mathematical operations
 
 2+3              # addition
           
@@ -408,11 +421,11 @@ c(3,6)
 
 ### Challenge 4: Create a 3 by 1 (3 rows, 1 column) matrix called 'd.mat3' with elements c(1,2,3). How is this matrix different from a vector (e.g., created by running the command '1:3')? HINT: use the "dim()" function, which returns the dimensionality of an object.
 
-### Challenge 5: Take your matrix 'd.mat2' and convert it to a vector containing all elements in the matrix. HINT: use the "as.vector()" function.
+### Challenge 5: Take your matrix 'd.mat2' and convert (coerce) it to a vector containing all elements in the matrix. HINT: use the "as.vector()" function.
 
 ### Challenge 6: Create a list named 'd.list' that is composed of a vector: 1:3, a matrix: matrix(1:6,nrow=3,ncol=2), and an array: array(1:24,dim=c(3,2,4)).
 
-### Challenge 7: Extract (subset) the 2nd row of the 3rd matrix in the array in 'd.list' (the list created in challenge 6 above).
+### Challenge 7: Extract (subset) the 2nd row of the 3rd matrix in the array in 'd.list' (the list created in challenge 6 above). For this exercise, please consider the array to be indexed as [row #, column #, matrix #]. 
 
 ### Challenge 8: (extra challenging!) Create a data frame named 'df_spatial' that contains 25 locations, with 'long' and 'lat' as the column names (25 rows/observations, 2 columns/variables). These locations should describe a 5 by 5 regular grid with extent long: [-1,1] and lat: [-1,1]. 
 
